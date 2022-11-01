7777464a17b50f491fb363651838fd86a72ea38e
Muschow <110130595+Muschow@users.noreply.github.com>
2022-11-01 18:57:12 +0000

    Add files via upload

---
+++ pacman 3.5/scripts/Movement.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class Movement : Node
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    public Dictionary<Vector2, int> nodeDict;
+    public Dictionary<Vector2, int> InitNodeDict()
+    {
+        TileMap mazeTm = GetNode<TileMap>("/root/Game/MazeContainer/Maze/MazeTilemap");
+        return nodeDict = (Dictionary<Vector2, int>)mazeTm.Get("nodeDict");
+    }
+
+    public void Dijkstras(Vector2 source, Vector2 target) //takes in graph (adjMatrix) and source (Pos) Ghost MUST spawn on node
+    {
+
+        Vector2[] distFromSource = new Vector2[nodeDict.Count];
+        distFromSource[nodeDict[source]] = Vector2.Zero;
+    }
+
+    // Called when the node enters the scene tree for the first time.
+    // public override void _Ready()
+    // {
+
+    // }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+}
